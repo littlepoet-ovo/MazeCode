b74bfe2b429bbce3e325f444e10ccbf518ba37ac
littlepoet-ovo <113732483+littlepoet-ovo@users.noreply.github.com>
2023-08-31 17:51:05 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,18 @@ import javax.swing.*;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Database d = new Database();
-        int link_state=d.link();
-        if(link_state==200){
-            new LoginUI(d);//进入登录页面
-        }else {
+    public static void main(String[] args) {
+        try {
+            Database d = new Database();
+            int link_state=d.link();
+            if(link_state==200){
+                new LoginUI(d);//进入登录页面
+            }else {
+                JOptionPane.showMessageDialog(null,"数据库连接失败！");
+            }
+        }catch (Exception e){
             JOptionPane.showMessageDialog(null,"数据库连接失败！");
         }
+
     }
 }
